39ea8d1be6b1ed74bd681ac74b4733051b725357
[Jae Park] <[jaepark052@gmail.com]>
2019-11-20 19:55:02 -0800

    added some design

--- src/components/add-course/AddCourse.tsx
+++ src/components/add-course/AddCourse.tsx
@@ -1,11 +1,23 @@
 import React, { useState } from 'react'
-import { makeStyles, createStyles } from '@material-ui/styles'
+import { makeStyles, createStyles, } from '@material-ui/styles'
 import { Theme, Button } from '@material-ui/core'
+import TextField from '@material-ui/core/TextField'
 
 
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
+        container: {
+            display: 'flex',
+            flexWrap: 'wrap',
+          },
+
+        textField:{
+            marginLeft: theme.spacing(1),
+            marginRight: theme.spacing(1),
+            width: 200,
+        },
+
         button: {
             display: 'grid',
         },
@@ -16,25 +28,47 @@ export const AddCourse: React.FC = () => {
     const classes = useStyles()
 
     const [ courseName, setCourseName ] = useState('')
+    const [ CRNnumber, setCRNnumber ] = useState('')
+    
     return (
+        <form className = {classes.container}>
         <div>
         Want to add a course?
         <br/>
         <br/>
             Course Name :
-            <input 
-                type="text" 
-                name="courseName" 
+            <TextField 
+                name="courseName"
+                className={classes.textField}
+                label="Course Name"
+                placeholder="e.g) CSC225"
                 value={courseName} 
                 onChange={(event) => {
-                setCourseName(event.target.value);
-                console.log(event.target.value)}
-                }/>
-        <br/>
-            CRN: <input type="text" name="CRNname" value=""/>
-        <br/>
+                    setCourseName(event.currentTarget.value)
+                }}/>
+            
+            <TextField 
+                name="CRNnumber"
+                className={classes.textField}
+                label="CRN"
+                placeholder="e.g) 12345"
+                value={CRNnumber} 
+                onChange={(event) => {
+                    setCRNnumber(event.currentTarget.value)
+                }}/>
+
+
+
         <Button>submit</Button>
+
+        {/* below, just testing if textfields are actually fetching the info*/}
+            
+        <p>course name: {courseName}</p>
+        <p>CRN: {CRNnumber}</p>
+
         </div>
+
+        </form>
     )
 }
 
