c0b7c4f9d4d201a7d2c23b655b2c30bd51c3eccc
Braiden Cutforth <braiden89@gmail.com>
2019-11-25 16:40:33 -0800

    Add mock data and mock functions (#29)

--- src/services/storage/index.ts
+++ src/services/storage/index.ts
@@ -1,21 +1,52 @@
-import { course1 } from './mock'
+import { user1 } from './mock'
+
+interface DateRange {
+    start: Date
+    end: Date
+}
+
+interface Fraction {
+    numerator: number
+    denominator: number
+}
 
 export interface Course {
     name: string
     destiredGrade: number
     items: CourseItem[]
+    crn?: number
+    dateRange?: DateRange
 }
 
 export interface CourseItem {
     weight: number
     name: string
-    grade?: number
+    grade?: number | Fraction
+    dueDate?: Date
 }
 
-export function store(course: Course) {
-    return Promise.resolve(course)
+export interface User {
+    name: string
+    courses: Course[]
+    userId: string
+    desiredGpa: number
 }
 
-export function load() {
-    return Promise.resolve(course1)
+/**
+ * Currently doesn't do anything, but can be used as a fake call to store user data
+ * @param id User id to store info for
+ * @param Partial<User> Partial user object for updating the user's info
+ */
+export function store(id: string, user: Partial<User>) {
+    ;[user, id] // no-op
+    return Promise.resolve('Success')
+}
+
+/**
+ * Currently set id to anything, and it will return mock data
+ * @param id User id
+ */
+export function loadUser(id: string) {
+    id // no-op
+    return Promise.resolve(user1)
 }
