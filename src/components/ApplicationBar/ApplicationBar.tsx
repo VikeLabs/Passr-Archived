e54d5403affd3074d62c84b5435ae7ed34ca3750
louis-kedziora <louie_101@live.ca>
2019-11-27 23:45:32 -0800

    Update src/components/ApplicationBar/ApplicationBar.tsx
    Co-Authored-By: Braiden Cutforth <braiden89@gmail.com>

--- src/components/ApplicationBar/ApplicationBar.tsx
+++ src/components/ApplicationBar/ApplicationBar.tsx
@@ -60,7 +60,6 @@ interface ApplicationBarProps{
 }
 
 export default function ApplicationBar() {
-  const { container } = props;
   const classes = useStyles();
   const theme = useTheme();
   const [mobileOpen, setMobileOpen] = React.useState(false);
