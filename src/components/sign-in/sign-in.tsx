71fd2fa11b1bb4c9c96178e8441f9b45767a5813
phender5 <paulyh1995@gmail.com>
2019-12-02 17:27:01 -0800

    - Added sign in button - Added vsconfig folder to gitignore - Added sign-in state to app.tsx

---
+++ src/components/sign-in/sign-in.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import color from '@material-ui/core/colors/deepOrange';
+import { Color } from 'csstype';
+
+interface Props {
+  isSignedIn: boolean
+  changeSignIn: () => void
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    button: {
+      margin: theme.spacing(1),
+    },
+    input: {
+      display: 'none',
+    },
+  }),
+);
+
+export default function SignInButton(props: Props) {
+  const classes = useStyles();
+  let buttonText = ''
+  let buttonColor: 'primary' | 'secondary' | undefined = undefined
+
+  if(props.isSignedIn) {
+    buttonText = 'Sign Out'
+    buttonColor = 'secondary'
+  } else {
+    buttonText = 'Sign In'
+    buttonColor = 'primary'
+  }
+
+  return (
+    <div>
+      <Button variant='contained' color={buttonColor} className={classes.button} onClick={props.changeSignIn}>
+        {buttonText}
+      </Button>
+    </div>
+  );
+}
