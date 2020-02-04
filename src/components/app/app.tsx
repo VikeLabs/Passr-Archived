e590465e7dd3f9697b0e0e736a14661f676c12dc
Cindy <cindyunrau@uvic.ca>
2020-02-03 18:45:31 -0800

    messing with app.tsx 2

---
+++ src/components/app/app.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import NavBar from '../nav/nav'
+import { makeStyles, Theme, createStyles, Grid } from '@material-ui/core'
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            display: 'grid',
+        },
+    }),
+)
+
+export const App: React.FC = () => {
+    const classes = useStyles()
+    return (
+        <div id="app" className={classes.root}>
+            <NavBar />
+            <h1>Passr</h1>
+            <p>Congratulation, you have successfully started Passr.</p>
+        </div>
+    )
+}
+
+export default App
