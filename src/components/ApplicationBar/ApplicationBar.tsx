d3a68f5a62877f28ebebd81e7aa39239d7a19ac3
[Jae Park] <[jaepark052@gmail.com]>
2020-02-06 22:02:24 -0800

    alignment change & debugging

--- src/components/ApplicationBar/ApplicationBar.tsx
+++ src/components/ApplicationBar/ApplicationBar.tsx
@@ -130,7 +130,6 @@ export default function ApplicationBar() {
         {/* Insert CourseContent here */}
         <Typography paragraph>
         </Typography>
-        <AddCourse addCourse={() => {}} />
       </main>
     </div>
   );
