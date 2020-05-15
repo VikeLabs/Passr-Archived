30551c18b40b668ccbd9832d469f74c740de62d9
braidencutforth <braiden89@gmail.com>
2020-05-14 18:37:47 -0700

    finish restructure

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { makeStyles, createStyles, Grid } from '@material-ui/core'
 import ApplicationBar from '../ApplicationBar/ApplicationBar'
 import { loadUser, User } from '../../services/storage'
+import { Features } from '../../services/features'
 
 const useStyles = makeStyles(() =>
     createStyles({
@@ -47,6 +48,7 @@ export const App: React.FC = () => {
     const classes = useStyles()
 
     const [user, setUser] = useState<User | null>(null)
+    const [selectedFeature, setSelectedFeature] = useState<Features>('calendar')
 
     useEffect(() => {
         loadUser('').then(user => setUser(user))
---
+++ src/services/features/index.ts
@@ -0,0 +1,1 @@
+export type Features = 'calendar' | 'duedates' | 'grades' | 'timetable'
