ca3c2f7ce1a718841c390eb33ea4de7f5dcd0749
Braiden Cutforth <braiden89@gmail.com>
2020-05-14 18:20:54 -0700

    28 mock data (#47)
    * Mock Data
    
    - update mock data to represent knew data scheme
    
    * fix mock data
    
    * add defaultSemester to user

--- src/services/storage/mock.ts
+++ src/services/storage/mock.ts
@@ -1,42 +1,30 @@
-import { Course, CourseItem, User } from './index'
+import { Course, User, Semester } from './index'
 
-const thisSemesterEnd = new Date('December 31, 2019')
-const thisSemesterStart = new Date('September 01, 2019')
-
-const lastSemesterEnd = new Date('August 31, 2019')
-const lastSemesterStart = new Date('May 01, 2019')
-
-const course1items: CourseItem[] = [
-    { name: 'Assignment 1', weight: 0.07, grade: 1 },
-    { name: 'Assignment 2', weight: 0.07, grade: 1 },
-    { name: 'Assignment 3', weight: 0.07 },
-    { name: 'Assignment 4', weight: 0.11 },
-    {
-        name: 'Midterm 1',
-        weight: 0.22,
-        grade: { numerator: 19, denominator: 22 },
-    },
-    {
-        name: 'Midterm 2',
-        weight: 0.22,
-        grade: { numerator: 20, denominator: 22 },
-    },
-    { name: 'Midterm 3', weight: 0.24 },
-]
-
-export const course1: Course = {
+const course1: Course = {
     name: 'CSC 370',
-    destiredGrade: 0.8,
-    items: course1items,
-    dateRange: {
-        start: new Date(thisSemesterStart),
-        end: new Date(thisSemesterEnd),
-    },
+    desiredGrade: 0.8,
+    items: [
+        { name: 'Assignment 1', weight: 0.07, grade: 1 },
+        { name: 'Assignment 2', weight: 0.07, grade: 1 },
+        { name: 'Assignment 3', weight: 0.07 },
+        { name: 'Assignment 4', weight: 0.11 },
+        {
+            name: 'Midterm 1',
+            weight: 0.22,
+            grade: { numerator: 19, denominator: 22 },
+        },
+        {
+            name: 'Midterm 2',
+            weight: 0.22,
+            grade: { numerator: 20, denominator: 22 },
+        },
+        { name: 'Midterm 3', weight: 0.24 },
+    ],
 }
 
 const course2: Course = {
     name: 'ECE 355',
-    destiredGrade: 0.75,
+    desiredGrade: 0.75,
     items: [
         { name: 'Midterm1', weight: 0.2 },
         { name: 'Assignment 1', weight: 0.5 / 6 },
@@ -48,15 +36,11 @@ const course2: Course = {
         { name: 'Lab', weight: 0.3 },
         { name: 'final', weight: 0.45 },
     ],
-    dateRange: {
-        start: new Date(thisSemesterStart),
-        end: new Date(thisSemesterEnd),
-    },
 }
 
 const course3: Course = {
     name: 'SENG 350',
-    destiredGrade: 0.8,
+    desiredGrade: 0.8,
     items: [
         { name: 'Quiz 1', weight: 0.02 },
         { name: 'Quiz 2', weight: 0.02 },
@@ -67,15 +51,11 @@ const course3: Course = {
         { name: 'Project', weight: 0.55 },
         { name: 'Class Participation', weight: 0.1 },
     ],
-    dateRange: {
-        start: new Date(thisSemesterStart),
-        end: new Date(thisSemesterEnd),
-    },
 }
 
 const course4: Course = {
     name: 'EPHE 155',
-    destiredGrade: 0.9,
+    desiredGrade: 0.9,
     items: [
         { name: 'Assignemnt 1', weight: 0.05 },
         { name: 'Assignment 2', weight: 0.1 },
@@ -85,15 +65,11 @@ const course4: Course = {
         { name: 'Midterm', weight: 0.25 },
         { name: 'Final', weight: 0.4 },
     ],
-    dateRange: {
-        start: new Date(lastSemesterStart),
-        end: new Date(lastSemesterEnd),
-    },
 }
 
 const course5: Course = {
     name: 'CSC 421',
-    destiredGrade: 0.75,
+    desiredGrade: 0.75,
     items: [
         { name: 'Assignment 1', weight: 0.05 },
         { name: 'Assignment 2', weight: 0.05 },
@@ -103,9 +79,20 @@ const course5: Course = {
     ],
 }
 
+const semester1: Semester = {
+    title: 'Fall 2019',
+    courses: [course1, course2, course3],
+}
+
+const semester2: Semester = {
+    title: 'Summer 2020',
+    courses: [course4, course5],
+}
+
 export const user1: User = {
     name: 'Burt Macklin',
-    courses: [course1, course2, course3, course4, course5],
+    semesters: [semester1, semester2],
     userId: '0ab128adf',
+    defaultSemester: 'Summer 2020', // Temp for maintaining current semester
     desiredGpa: 7,
 }
