bf68ae18089dc95920d01cca2826e57e9544fdb0
Braiden Cutforth <braiden89@gmail.com>
2020-05-18 15:31:20 -0700

    Initial commit for the topbar
    Setup spacing for different areas of app
    Added a material-ui theme provider to the app
    Create topbar component for selecting different options
    - Initial idea is for selecting different courses
    - Supports other kinds of selection since it is a controlled  component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from 'react'
-import { makeStyles, createStyles, Grid } from '@material-ui/core'
-import ApplicationBar from '../ApplicationBar/ApplicationBar'
+import { makeStyles, createStyles } from '@material-ui/core'
 import { loadUser, User } from '../../services/storage'
 import { Features } from '../../services/features'
+import TopBar from '../TopBar/TopBar'
 
-const useStyles = makeStyles(() =>
+const useStyles = makeStyles(theme =>
     createStyles({
         root: {
             display: 'grid',
             gridTemplateColumns: '1.5fr 7fr 1fr',
             width: '100vw',
             height: '100vh',
+            boxSizing: 'border-box',
             gridTemplateRows: '1fr 10fr',
             gridTemplateAreas: `"sidebar topbar account"
                                 "sidebar content content"`,
+            backgroundColor: '#edf6ff',
+            rowGap: `${theme.spacing(1)}px`,
+            columnGap: `${theme.spacing(1)}px`,
+            padding: theme.spacing(1),
         },
 
         sidebar: {
@@ -23,7 +28,7 @@ const useStyles = makeStyles(() =>
 
         topbar: {
             gridArea: 'topbar',
-            backgroundColor: 'cyan',
+            // backgroundColor: 'cyan',
         },
 
         account: {
@@ -38,17 +43,12 @@ const useStyles = makeStyles(() =>
     }),
 )
 
-const drawerContent = (
-    <div>
-        <h1>A drawer</h1>
-    </div>
-)
-
 export const App: React.FC = () => {
     const classes = useStyles()
 
     const [user, setUser] = useState<User | null>(null)
     const [selectedFeature, setSelectedFeature] = useState<Features>('calendar')
+    const [topBarSelection, setTopBarSelection] = useState(0) // Needs to be reset on feature change
 
     useEffect(() => {
         loadUser('').then(user => setUser(user))
@@ -57,7 +57,13 @@ export const App: React.FC = () => {
     return (
         <div id="app" className={classes.root}>
             <div id="sidebar" className={classes.sidebar}></div>
-            <div id="topbar" className={classes.topbar}></div>
+            <div id="topbar" className={classes.topbar}>
+                <TopBar
+                    options={undefined}
+                    selection={topBarSelection}
+                    onChange={setTopBarSelection}
+                />
+            </div>
             <div id="account" className={classes.account}></div>
             <div id="content" className={classes.content}></div>
         </div>
